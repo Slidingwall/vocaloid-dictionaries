91e47600c6e18d72d7b4b3788bfa5a0870fcaebe
滑墙 <60685118+Slidingwall@users.noreply.github.com>
2022-01-17 07:41:52 +0800

    Add files via upload

--- Chn2Eng.lua
+++ Chn2Eng.lua
@@ -3,11 +3,11 @@
 
 function manifest()
     myManifest = {
-        name          = "Chn2Eng",
+        name          = "Chinese to English",
         comment       = "Convert Chinese VSQX for English voice banks",
         author        = "Slidingwall",
-        pluginID      = "{A03FEF7A-4524-4446-A12F-92E54EE5D41T}",
-        pluginVersion = "1.0.0.0",
+        pluginID      = "{A03FEF7A-1234-3456-A12F-92E54EE5D41T}",
+        pluginVersion = "For English Libraries",
         apiVersion    = "3.0.1.0"
     }
     
@@ -39,7 +39,7 @@ function main(processParam, envParam)
 	mapping["u"]	= "u:"
 	mapping["y"]	= "j u:"
 	mapping["@`"]	= "@r"
-	mapping["i\"]	= "I"
+	mapping["i\\"]	= "I"
 	mapping["i`"]	= "u:"
 	mapping["aI"]	= "aI"
 	mapping["ei"]	= "eI"
@@ -62,21 +62,21 @@ function main(processParam, envParam)
 	mapping["u@_n"]	= "w @ n"
 	mapping["y_n"]	= "j u: n"
 	mapping["y{_n"]	= "j u: { n"
-	mapping["AN"]	= "{ N"
+	mapping["AN"]	= "V N"
 	mapping["@N"]	= "@ N"
 	mapping["iN"]	= "i: N"
-	mapping["iAN"]	= "j { N"
-	mapping["uAN"]	= "w { N"
+	mapping["iAN"]	= "j V N"
+	mapping["uAN"]	= "w V N"
 	mapping["u@N"]	= "w @ N"
-	mapping["UN"]	= "U N"
-	mapping["iUN"]	= "j U N"
+	mapping["UN"]	= "@ U N"
+	mapping["iUN"]	= "j @ U N"
 	mapping["p_h"]	= "ph"
 	mapping["t_h"]	= "th"
 	mapping["k_h"]	= "kh"
 	mapping["x"]	= "h"
-	mapping["ts\"]	= "dZ"
-	mapping["ts\_h"]	= "tS"
-	mapping["s\"]	= "S"
+	mapping["ts\\"]	= "dZ"
+	mapping["ts\\_h"]	= "tS"
+	mapping["s\\"]	= "S"
 	mapping["ts`"]	= "dZ"
 	mapping["ts`_h"]	= "tS"
 	mapping["s`"]	= "S"
@@ -86,6 +86,7 @@ function main(processParam, envParam)
 
 	mapping["Sil"]	= "Sil"
 
+	-- get list of notes
 	VSSeekToBeginNote()
 	idx = 1
 	retCode, noteEx = VSGetNextNoteEx()
@@ -94,34 +95,43 @@ function main(processParam, envParam)
 		retCode, noteEx = VSGetNextNoteEx()
 		idx = idx + 1
 	end
-
+	
+	-- check if empty
 	noteCount = table.getn(noteExList)
 	if (noteCount == 0) then
 		VSMessageBox("No notes to process!", 0)
 		return 0
 	end
 	
+	-- check Job Plugin can lock phoneme
+	local bPhLock = canPhLock(noteExList[1])
+	
+	-- transform notes
 	for idx = 1, noteCount do
 		local updNoteEx = {}
 		updNoteEx = noteExList[idx]
 		
+		-- split on whitespace
 		local phns = {}
 		for token in string.gmatch(updNoteEx.phonemes, "[^%s]+") do
 			 table.insert(phns, token)
 		end
 		
+		-- do phoneme-by-phoneme mapping
 		for i = 1, table.getn(phns) do
+			-- try exceptions first
 			local mapped = false
 			if i+1 <= table.getn(phns) then
 				twophn = phns[i] .. " " .. phns[i+1]
 				if mapping2[twophn] then
 					phns[i] = mapping2[twophn][1]
 					phns[i+1] = mapping2[twophn][2]
-
+					-- (in this case no need to increment i because mapping2[twophn][2] is always "i", so will map to "i" on next iteration)
 					mapped = true -- there's no continue in lua
 				end
 			end
 			
+			-- general mapping second
 			if not mapped then
 				if mapping[phns[i]] then
 					phns[i] = mapping[phns[i]]
@@ -129,8 +139,15 @@ function main(processParam, envParam)
 			end
 		end
 		
+		-- join by space
 		updNoteEx.phonemes = table.concat(phns, " ");
 		
+		-- Do phoneme lock
+		if (bPhLock) then
+			updNoteEx.phLock = 1
+		end		
+		
+		-- update note
 		retCode = VSUpdateNoteEx(updNoteEx);
 		if (retCode ~= 1) then
 			VSMessageBox("Failed to update note!", 0)
@@ -138,5 +155,20 @@ function main(processParam, envParam)
 		end
 	end
 
+	-- Success
 	return 0
+end
+
+-- Check if note has phLock property (only in newer API, Editor 3.0.5+)
+function canPhLock(note)
+	local status = false
+
+	for key, val in pairs(note) do
+		if (key == "phLock") then
+			status = true
+			break
+		end
+	end
+	
+	return status
 end
