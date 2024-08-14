4047a23230dcc7f20e75ec86c6f268a26694cd0b
滑墙 <whq20011120@hotmail.com>
2024-08-14 21:26:21 +0800

    Update 通用跨语种插件拆音版General Cross-language Plugin with Spliting.lua

--- 通用跨语种插件拆音版General Cross-language Plugin with Spliting.lua
+++ 通用跨语种插件拆音版General Cross-language Plugin with Spliting.lua
@@ -12,7 +12,9 @@ end
 
 function split(str, delimiter)  
     local result = {}  
-    string.gsub(str, '[^' .. delimiter .. ']+', function(w) table.insert(result, w) end)  
+    for word in string.gmatch(str, '[^' .. delimiter .. ']+') do  
+        table.insert(result, word)  
+    end  
     return result  
 end
 
@@ -38,12 +40,10 @@ function loadDicts(file)
     for line in io.lines(file) do  
         local lineData = split(line,",")
         if prefixToDict[lineData[1]] then  
-            local dict = prefixToDict[lineData[1]]  
-            dict[lineData[2]] = lineData[3]
+            prefixToDict[lineData[1]][lineData[2]] = lineData[3]
         end    
     end  
-
-    return   dicts
+    return dicts
 end  
 
 function splitNote(str,phn,dictphn)  
@@ -73,18 +73,9 @@ function copyNoteEx(src)
     return new
 end
 
-function main(processParam, envParam)
-    local beginPosTick = processParam.beginPosTick
-    local endPosTick = processParam.endPosTick
-    local songPosTick = processParam.songPosTick
-    
-    local scriptDir = envParam.scriptDir
-    local scriptName = envParam.scriptName
-    local tempDir = envParam.tempDir
-    
+function main(processParam, envParam) 
     local noteEx = {}
     local noteExList = {}
-    local noteCount
     local retCode
     local idx
     
@@ -126,27 +117,22 @@ function main(processParam, envParam)
         idx = idx + 1
     end
     
-    noteCount = #noteExList
-    if (noteCount == 0) then
+    if (#noteExList == 0) then
         VSMessageBox("No notes to process!", 0)
         return 0
     end
     
-    for idx = 1, noteCount do
+    for idx = 1, #noteExList do
         local updNoteEx = noteExList[idx]
-        local splNoteEx=copyNoteEx(updNoteEx)
-        local phns = {}
         local splphn
         local updphn
         local splitflag = 0
-        for token in string.gmatch(updNoteEx.phonemes, "[^%s]+") do
-            table.insert(phns, token)
-        end
+        local phns = split(updNoteEx.phonemes, " ")
         
-        for i = 1, #phns do
+        for phnIdx = 1, #phns do
             local mapped = false
-            if i + 2 <= #phns then
-                threephn = phns[i] .. " " .. phns[i + 1] .. " " .. phns[i + 2]
+            if phnIdx + 2 <= #phns then
+                threephn = table.concat(phns[phnIdx:phnIdx+2], " ")
                 if dicts.l3[threephn] then
                     splphn,updphn=splitNote(updNoteEx.phonemes,threephn,dicts.l3[threephn]) 
                     splitflag = 1
@@ -158,15 +144,15 @@ function main(processParam, envParam)
                     mapped = true
                 end
                 if not mapped and dicts.mp3[threephn] then
-                    phns[i] = dicts.mp3[threephn]
-                    phns[i + 1] = ""
-                    phns[i + 2] = ""
+                    phns[phnIdx] = dicts.mp3[threephn]
+                    phns[phnIdx + 1] = ""
+                    phns[phnIdx + 2] = ""
                     mapped = true
                 end
             end
 
-            if not mapped and i + 1 <= #phns then
-                twophn = phns[i] .. " " .. phns[i + 1] 
+            if not mapped and phnIdx + 1 <= #phns then
+                twophn = table.concat(phns[phnIdx:phnIdx+1], " ")
                 if dicts.l2[twophn] then
                     splphn,updphn=splitNote(updNoteEx.phonemes,twophn,dicts.l2[twophn]) 
                     splitflag = 1
@@ -178,14 +164,14 @@ function main(processParam, envParam)
                     mapped = true
                 end
                 if not mapped and dicts.mp2[twophn] then
-                    phns[i] = dicts.mp2[twophn]
-                    phns[i + 1] = ""
+                    phns[phnIdx] = dicts.mp2[twophn]
+                    phns[phnIdx + 1] = ""
                     mapped = true
                 end
             end
 
-            if not mapped and dicts.mp1[phns[i]] then
-                phns[i] = dicts.mp1[phns[i]]
+            if not mapped and dicts.mp1[phns[phnIdx]] then
+                phns[phnIdx] = dicts.mp1[phns[phnIdx]]
             end
         end
         
@@ -194,6 +180,7 @@ function main(processParam, envParam)
             updNoteEx.phLock = 1
             retCode = VSUpdateNoteEx(updNoteEx)
         elseif splitflag == 1 then
+            local splNoteEx=copyNoteEx(updNoteEx)
             updNoteEx.phonemes = updphn
             updNoteEx.phLock = 1
             updNoteEx.posTick = splNoteEx.posTick + 30
@@ -203,6 +190,7 @@ function main(processParam, envParam)
             splNoteEx.durTick = 30
             retCode = VSUpdateNoteEx(updNoteEx) and VSInsertNoteEx(splNoteEx)
         elseif splitflag == 2 then
+            local splNoteEx=copyNoteEx(updNoteEx)
             updNoteEx.phonemes = updphn
             updNoteEx.phLock = 1
             updNoteEx.durTick = splNoteEx.durTick - 30
