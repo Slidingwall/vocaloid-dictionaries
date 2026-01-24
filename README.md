e11a2410be3028e571e3ae10a0d24b05c1a29580
滑墙 <whq20011120@hotmail.com>
2026-01-25 00:29:55 +0800

    Update Spanish lyric filling logic and README
    Refactored and optimized the Spanish syllabification and phoneme conversion logic in Spanish.lua for improved accuracy and maintainability. Updated the README to clarify plugin changes, add credits for language logic sources, and revise plugin feature descriptions.

--- README.md
+++ README.md
@@ -9,10 +9,13 @@ Visit our [GitHub Pages](https://slidingwall.github.io/vocaloid-dictionaries), o
 Demos can be listened right [here](https://slidingwall.github.io/vocaloid-dictionaries/demo).
 音频样本可以在[这里](https://slidingwall.github.io/vocaloid-dictionaries/demo)听到。
 
+**Due to the update of new features, former plugins have now been moved to the `archived` folder.**  
+**由于新功能的更新，先前的插件现已移至`archived`文件夹中。**
+
 ## Universal Lyric Fliiling Plugin 万能填词插件
 
-(WIP)Because I forgot the development progress, support for some languages may not be complete yet.  
-(开发中)由于我忘记了开发进度，部分语言的支持可能尚未完成。  
+(WIP)Because I forgot the development progress, support for some languages may not be complete yet. English are not supported yet.  
+(开发中)由于我忘记了开发进度，部分语言的支持可能尚未完成。暂时不支持英语。  
 
 This plugin is modified from the user dictionary plugin and serves as an implementation of a third-party Grapheme-to-Phoneme system. It enables cross-lingual conversion directly during the lyrics input process.   
 这个插件基于用户字典插件修改而来，是一个第三方单词到音素系统的实现，用于直接在填词的过程中完成跨语种转换的操作。  
@@ -20,26 +23,13 @@ This plugin is modified from the user dictionary plugin and serves as an impleme
 The plugin will automatically detect the singer's language and the number of available notes for lyrics input. After entering the lyrics, you need to manually select the language of the entered lyrics. The plugin will then handle the remaining conversion tasks.  
 插件将会自动识别歌手语言和可填词音符的个数。填入歌词后，您需要手动选择填入歌词的语言，随后由插件完成剩余的转换工作。  
 
-## General Cross-language Plugin SE 通用跨语种插件SE
+The logic for Spanish is ported from [silabajs](https://github.com/nicofrem/silabajs), and the logic for Korean is ported from [hangul-sound-shift](https://github.com/QikLiang/hangul-sound-shift).  
+西班牙语逻辑基于[silabajs](https://github.com/nicofrem/silabajs)，韩语逻辑基于[hangul-sound-shift](https://github.com/QikLiang/hangul-sound-shift)。  
 
-We strongly recommend that you use general Job-Plugin to simplify operations, General Job-Plugin greatly simplified usage.  
-我们强烈建议您使用通用插件以简化操作，通用插件的出现大大简化了操作步骤。  
+## General Cross-language Plugin SE 通用跨语种插件SE  
 
-**Due to the update of new features, former plugins have now been moved to the `archived` folder.**  
-**由于新功能的更新，先前的插件现已移至`archived`文件夹中。**   
-
-Building on the [General cross-lingual Job Plugin(Chinese Page)](https://www.bilibili.com/read/cv7732403/) created by [atonyxu](https://github.com/atonyxu), we have modified it into another version (referred to as SE, the second edition). This new version allows the dictionary to only provide simple conversion rules, while leaving the checking of unsupported CV pronunciations and phoneme splitting to the plugin automatically.  
-我们在[atonyxu](https://github.com/atonyxu)制作的[通用跨语种插件](https://www.bilibili.com/read/cv7732403/)的基础上再次修改了一版插件（称之为SE，第二版），让字典仅提供简单的转换规则，将不支持的CV发音与拆音交由插件自动检查。
-
-|VB \ Lyrics |CHN Lyrics|ENG Lyrics|JPN Lyrics|KOR Lyrics|ESP Lyrics|
-|:----:|:----:|:----:|:----:|:----:|:----:|
-|CHN VB|N/A|√|√|√|√|
-|ENG VB|√|N/A|√|√|√|
-|JPN VB|√|√|N/A|√|v|
-|KOR SeeU|√|√|√|N/A|√|
-|KOR UNI|√|√|√|N/A|√|
-|ESP VB|√|√|√|√|N/A|
-|MAIKA|√|√|√|√|N/A|
+Building on the [General cross-lingual Job Plugin(Chinese Page)](https://www.bilibili.com/read/cv7732403/) created by [atonyxu](https://github.com/atonyxu), we have modified it into another version (referred to as SE, the second edition). This new version allows the dictionary to only provide simple conversion rules, while leaving the checking of unsupported CV pronunciations and phoneme splitting(WIP) to the plugin automatically.  
+我们在[atonyxu](https://github.com/atonyxu)制作的[通用跨语种插件](https://www.bilibili.com/read/cv7732403/)的基础上再次修改了一版插件（称之为SE，第二版），让字典仅提供简单的转换规则，将不支持的CV发音与拆音（开发中）交由插件自动检查。
 
 ## Dictionaries for General user dictionary Job Plugin 用户字典插件字典
 
@@ -56,7 +46,7 @@ Folder `/dict-udc`is compatible with [General user dictionary Job Plugin(Chinese
 ## Other Plugins 其他插件
 ### Find & Replace Phonemes Job Plugin 音素查找与替换插件
 
-Based on the official Yamaha `convertPhonemes.lua`, an option for perfect matching has been added.
+Based on the official Yamaha `convertPhonemes.lua`, an option for perfect matching has been added.  
 基于Yamaha官方的`convertPhonemes.lua`，增加了一个用于完全匹配的选项。
 
 **Any contributions are welcome. / 欢迎任何形式的贡献**  
--- lyric-filling/Spanish.lua
+++ lyric-filling/Spanish.lua
@@ -1,148 +1,101 @@
 return function(lyrics, idx, _)
-    local strong = {a=true, e=true, o=true, ["á"]=true, ["é"]=true, ["ó"]=true}
-    local vowels = setmetatable({i=true, u=true, ["í"]=true, ["ú"]=true, ["ü"]=true},{__index = strong})
-    local voiced = {b=true, v=true, g=true, p=true, d=true, c=true, k=true, f=true, t=true}
-    local consonants = setmetatable({h=true, j=true, l=true, m=true, n=true, q=true, r=true, s=true, w=true, x=true, y=true, z=true,["ç"]=true},{__index = voiced})
-    local clusters = {pt=true, ct=true, cn=true, ps=true, mn=true, gn=true, ft=true, pn=true, cz=true, tz=true, ts=true}
-    local special = {s=true, l=true, r=true, n=true, c=true}
-    local replace = {["á"] = "a", ["é"] = "e", ["í"] = "i", ["ó"] = "o", ["ú"] = "u"}
     local lyricList, phonemeList = {}, {}
-    local def = idx == 2 and "u:" or "a"
     if idx == 7 then idx = 4 end
     local rules = {
-        { "([aeiou])x([aeiou])", "%1ks%2" },
-        { "([a-zñüç])", "%1 " },
-        { "x", "s" },
-        { "g e", "x e" },
-        { "g i", "x i" },
-        { "g u e", "g e" },
-        { "g u i", "g i" },
-        { "q u e", "k e" },
-        { "q u i", "k i" },
-        { "ç", "T" },
-        { "c e", "T e" },
-        { "c i", "T i" },
-        { "c ([^h])", "k %1" },
-        { "c h", "tS" },
-        { "j", "x" },
-        { "h", "" },
-        { "l l", "L" },
-        { "ñ", "J" },
-        { "r r", "rr" },
-        { "([ nls]) r", "%1 rr" },
-        { "v", "b" },
-        { "ü", "w" },
-        { "i ([aeou])", "j %1" },
-        { "u ([aeio])", "w %1" },
-        { "z", "T" },  
-        { "y", "jj" }, 
-        { "q", "k" },
+        { "([aeiou])x([aeiou])", "%1ks%2" },{ "([a-zñüç])", "%1 " },{ "x", "s" },{ "g e", "x e" },{ "g i", "x i" },
+        { "g u e", "g e" },{ "g u i", "g i" },{ "q u e", "k e" },{ "q u i", "k i" },{ "ç", "T" },
+        { "c e", "T e" },{ "c i", "T i" },{ "c ([^h])", "k %1" },{ "c h", "tS" },{ "j", "x" },
+        { "h", "" },{ "l l", "L" },{ "ñ", "J" },{ "r r", "rr" },{ "([ nls]) r", "%1 rr" },
+        { "v", "b" },{ "ü", "w" },{ "i ([aeou])", "j %1" },{ "u ([aeio])", "w %1" },{ "z", "T" },
+        { "y", "jj" },{ "q", "k" }
     }
     local dipthong = {
         [1]={ --Japanese
             ["([ktm4gdbp]) j"] = "%1'",["n j"] = "J",["h j"] = "C",["p\\ j"] = "p\\'",
             ["([ktm4gdbp]) i"] = "%1' i",["n i"] = "J i",["h i"] = "C i",["p\\ i"] = "p\\' i"
         },
-        [2]={["{ i:"]="aI",["e i:"]="eI",["O: i:"]="OI",["{ u:"]="aU",["e u:"]="@U",["O: u:"]="@U"},
-        [3]={["j ([aeou])"] = "j%1",["w ([ei])"] = "u%1",["w a"] = "oa",["w o"] = "u7"},
-        [4]={["j ([aeou])"] = "j%1",["w ([ei])"] = "u%1",["w a"] = "oa",["w o"] = "u7"},
-        [5]={
+        [2]={["{ i:"]="aI",["e i:"]="eI",["O: i:"]="OI",["{ u:"]="aU",["e u:"]="@U",["O: u:"]="@U"}, --English
+        [3]={["j ([aeou])"] = "j%1",["w ([ei])"] = "u%1",["w a"] = "oa",["w o"] = "u7"}, --Korean
+        [4]={["j ([aeou])"] = "j%1",["w ([ei])"] = "u%1",["w a"] = "oa",["w o"] = "u7"}, --SeeU
+        [5]={ --Chinese
             ["j a"]="ia",["j ei"]="iE_r",["j @U"]="iAU",["j u"]="i@U",
             ["w a"]="ua",["w ei"]="uei",["w i"]="u i",["w @U"]="uo",
             ["a i"]="aI",["ei i"]="ei",["a u"]="AU",["ei u"]="@U",["@U u"]="@U",
             ["w a i"]="uaI",["w ei i"]="uei",["i a u"]="iAU",["i e u"]="i@U",["i @U u"]="i@U"}
     }[idx]
     local dict = { -- Japanese,English,Korean,SeeU,Chinese
-        a={"a","{","a","a","a"},
-        b={"b","b","b","b","p"},
-        d={"d","d","d","d","t"},
-        e={"e","e","e","e","ei"},
-        f={"p\\","f","h","f","f"},
-        g={"g","g","g","g","k"},
-        i={"i","i:","i","i","i"},
-        j={"j","j","j","j","j"},
-        k={"k","k","k","k","k_h"},
-        l={"4","l0","l","l","l"},
-        m={"m","m","m","m","m"},
-        n={"n","n","n","n","n"},
-        J={"J","n j","n j","n j","n j"},
-        o={"o","O:","o","o","@U"},
-        p={"p","p","p","p","p_h"},
-        r={"4","r","r","r","l"},
-        s={"s","s","s","s","s"},
-        t={"t","t","t","t","t_h"},
-        T={"ts","T","s","s","ts"},
-        tS={"tS","tS","c","c","ts`"},
-        u={"M","u:","u","u","u"},
-        w={"w","w","w","w","w"},
-        x={"h","h","h","h","x"},
-        L={"j","j","j","j","j"},
-        jj={"j","j","j","j","j"},
-        rr={"4","r","r","r","l"},
+        a={"a","{","a","a","a"},b={"b","b","b","b","p"},d={"d","d","d","d","t"},e={"e","e","e","e","ei"},f={"p\\","f","h","f","f"},
+        g={"g","g","g","g","k"},i={"i","i:","i","i","i"},j={"j","j","j","j","j"},k={"k","k","k","k","k_h"},l={"4","l0","l","l","l"},
+        m={"m","m","m","m","m"},n={"n","n","n","n","n"},J={"J","n j","n j","n j","n j"},o={"o","O:","o","o","@U"},p={"p","p","p","p","p_h"},
+        r={"4","r","r","r","l"},s={"s","s","s","s","s"},t={"t","t","t","t","t_h"},T={"ts","T","s","s","ts"},tS={"tS","tS","c","c","ts`"},
+        u={"M","u:","u","u","u"},w={"w","w","w","w","w"},x={"h","h","h","h","x"},L={"j","j","j","j","j"},jj={"j","j","j","j","j"},rr={"4","r","r","r","l"},
     }
     lyrics=lyrics:gsub("[ÁÉÍÓÚÑÜÇ]", {["Á"] = "á", ["É"] = "é", ["Í"] = "í", ["Ó"] = "ó", ["Ú"] = "ú",["Ñ"] = "ñ", ["Ü"] = "ü", ["Ç"] = "ç"}):lower()
     for word in lyrics:gmatch("[a-záéíóúñüç%-]+") do
-        if word == "-" then
-            table.insert(lyricList, "-")
-            table.insert(phonemeList, "-")
+        if word == "-" then table.insert(lyricList, "-"); table.insert(phonemeList, "-")
         else
-            local pos = 1
-            while pos <= #word do
-                local syllableStart = pos
-                local lastConsonant = ""
-                while pos <= #word and consonants[word:sub(pos, pos)] and word:sub(pos, pos) ~= "y" do
-                    lastConsonant = word:sub(pos, pos)
-                    pos = pos + 1
+            local len, pos = #word, 1
+            while pos <= len do
+                local start, lastCon = pos, nil
+                while pos <= len and not "aeiouáéíóúü":find(word:sub(pos,pos), 1, true) and word:sub(pos,pos) ~= "y" do
+                    lastCon = word:sub(pos,pos); pos = pos + 1
                 end
-                if pos <= #word - 1 then
-                    if word:sub(pos, pos) == "u" then
-                        if lastConsonant == "q" or (lastConsonant == "g" and (word:sub(pos+1, pos+1) == "e" or word:sub(pos+1, pos+1) == "é" or word:sub(pos+1, pos+1) == "i" or word:sub(pos+1, pos+1) == "í")) then
-                            pos = pos + 1
+                if pos <= len then
+                    local char = word:sub(pos,pos)
+                    if char == "u" and lastCon == "q" then pos = pos + 1
+                    elseif char == "u" and lastCon == "g" and pos+1 <= len and "eiéí":find(word:sub(pos+1,pos+1),1,true) then pos = pos + 1
+                    elseif char == "ü" and lastCon == "g" then pos = pos + 1 end
+                end
+                if pos < len and word:sub(pos,pos) == "y" then pos = pos + 1 end
+                if pos > len then goto nextSyll end
+                local char = word:sub(pos,pos)
+                if "aeoáéó":find(char,1,true) then pos = pos + 1
+                elseif "iuíúü":find(char,1,true) then pos = pos + 1 end
+                local hasH = pos <= len and word:sub(pos,pos) == "h"
+                if hasH then pos = pos + 1 end
+                if pos <= len then
+                    char = word:sub(pos,pos)
+                    if "aeoáéó":find(char,1,true) then
+                        if "aeoáéó":find(word:sub(pos-1,pos-1),1,true) then pos = hasH and pos-1 or pos; goto endNuc
+                        else pos = pos + 1 end
+                    elseif "iuíúü":find(char,1,true) then
+                        if pos < len-1 and "aeiouáéíóúü":find(word:sub(pos+1,pos+1),1,true) then
+                            pos = word:sub(pos-1,pos-1)=="h" and pos-1 or pos; goto endNuc
                         end
-                    elseif word:sub(pos, pos) == "ü" and lastConsonant == "g" then
-                        pos = pos + 1
+                        pos = (char ~= word:sub(pos-1,pos-1)) and pos+1 or pos; goto endNuc
                     end
                 end
-                if pos <= #word and vowels[word:sub(pos, pos)] then
-                    local isStrong = strong[word:sub(pos, pos)]
-                    pos = pos + 1
-                    if pos <= #word and word:sub(pos, pos) == "h" then
-                        pos = pos + 1
-                    end
-                    if pos <= #word and vowels[word:sub(pos, pos)] then
-                        local nextIsStrong = strong[word:sub(pos, pos)]
-                        if nextIsStrong and isStrong then break else pos = pos + 1 end
-                    end
-                    if pos <= #word and (word:sub(pos, pos) == "i" or word:sub(pos, pos) == "u") then
-                        pos = pos + 1
-                    end
-                end
-                if pos <= #word and consonants[word:sub(pos, pos)] then
-                    if pos == #word then
-                        pos = pos + 1
-                    elseif not consonants[word:sub(pos+1, pos+1)] then
-                        break
+                if pos <= len and "iu":find(word:sub(pos,pos),1,true) then pos = pos + 1 end
+                ::endNuc::
+                if pos >= len or "aeiouáéíóúü":find(word:sub(pos,pos),1,true) then goto nextSyll end
+                if pos == len-1 then pos = pos + 1; goto nextSyll end
+                local char2 = word:sub(pos+1,pos+1)
+                if "aeiouáéíóúü":find(char2,1,true) then goto nextSyll end
+                local char1 = word:sub(pos,pos)
+                if pos < len-2 then
+                    local char3 = word:sub(pos+2,pos+2)
+                    if not "aeiouáéíóúü":find(char3,1,true) then
+                        if pos+3 == len then
+                            pos = (char2=="y" and "slrnc":find(char1,1,true)) and pos or (char3=="y" and pos+1 or pos+3); goto nextSyll
+                        end
+                        if char2 == "y" then pos = "slrnc":find(char1,1,true) and pos or pos+1; goto nextSyll end
+                        if "pt ct cn ps mn gn ft pn cz tz ts":find(char2..char3,1,true) then pos = pos+1; goto nextSyll end
+                        pos = ("lr":find(char3,1,true) or (char2=='c' and char3=='h') or char3=="y") and pos+1 or pos+2; goto nextSyll
                     else
-                        local c1, c2 = word:sub(pos, pos), word:sub(pos+1, pos+1)
-                        if pos < #word - 2 then
-                            local c3 = word:sub(pos+2, pos+2)
-                            if not vowels[c3] then
-                                pos = pos + (pos + 3 == #word + 1 and (c2 == "y" and (special[c1] and 0 or 1) or 3) or (c2 == "y" and (special[c1] and 0 or 1) or (clusters[c2..c3] and 1 or 2)))
-                            else
-                                local pair = c1..c2
-                                pos = pos + ((pair == "ll" or pair == "ch" or pair == "rr" or (c2 == "h" and not (c1 == "s" or c1 == "r")) or (c2 == "y" and special[c1]) or (voiced[c1] and (c2 == "l" or c2 == "r"))) and 0 or 1)
-                            end
-                        else
-                            pos = pos + 2
-                        end
+                        if ("ll ch rr"):find(char1..char2,1,true) or (char1~="sr" and char2=="h") then goto nextSyll end
+                        if char2 == "y" then pos = "slrnc":find(char1,1,true) and pos or pos+1; goto nextSyll end
+                        if "bvckfgpt":find(char1,1,true) and "lr":find(char2,1,true) then goto nextSyll end
+                        pos = pos + 1; goto nextSyll
                     end
+                else
+                    pos = char2 == "y" and pos or pos+2; goto nextSyll
                 end
-                table.insert(lyricList, word:sub(syllableStart, pos - 1))
-                local result=word:sub(syllableStart, pos - 1):gsub("[áéíóú]",replace)
-                for _, rule in ipairs(rules) do
-                    result = result:gsub(rule[1], rule[2])
-                end
-                result = result:gsub("%s+", " "):gsub("^%s+", ""):gsub("%s+$", ""):gsub("%S+", function(phoneme) return dict[phoneme][idx] or def end)
+                ::nextSyll::
+                local syllable = word:sub(start, pos - 1)
+                table.insert(lyricList, syllable)
+                local result=syllable:gsub("[áéíóú]",{["á"] = "a", ["é"] = "e", ["í"] = "i", ["ó"] = "o", ["ú"] = "u"})
+                for _, rule in ipairs(rules) do result = result:gsub(rule[1], rule[2]) end
+                result = result:gsub("%s+", " "):gsub("^%s+", ""):gsub("%s+$", ""):gsub("%S+", function(phoneme) return dict[phoneme][idx] or (idx == 2 and "u:" or "a") end)
                 for p, r in pairs(dipthong) do result = result:gsub(p, r) end
                 table.insert(phonemeList,result)
             end
